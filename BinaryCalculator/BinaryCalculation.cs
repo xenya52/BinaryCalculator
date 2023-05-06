89de9217367c2ac828e7e0723358f14d8bc8fb89
Lucy <108221381+Xennoxx@users.noreply.github.com>
2023-05-06 17:58:49 +0200

    need to commit because i will work from another defice l.o.l and the note that i addet is important

--- BinaryCalculator/BinaryCalculation.cs
+++ BinaryCalculator/BinaryCalculation.cs
@@ -1,5 +1,9 @@
 namespace BinaryCalculator;
-
+ 
+/*
+ * Example
+ * (110100101)₂ = (1 × 2⁸) + (1 × 2⁷) + (0 × 2⁶) + (1 × 2⁵) + (0 × 2⁴) + (0 × 2³) + (1 × 2²) + (0 × 2¹) + (1 × 2⁰) = (421)₁₀
+ */
 public class BinaryCalculation : UserInput
 {
     public int solutionOutput { get; set; } = 0;
@@ -8,17 +12,18 @@ public class BinaryCalculation : UserInput
     public void CalculationMethod(string userBinaryInput)
     {
         
-        char[] charArray = userBinaryInput.ToString().ToCharArray();
+        char[] charArray = userBinaryInput.ToCharArray();
+        Array.Reverse(charArray);
         int count = 1;
         int[] SolutionArray = new int[userBinaryInput.Length];
-        
-        Array.Reverse(charArray);
 
         for (int i = 0; i < userBinaryInput.Length; i++)
         {
-            count = count * 2; 
-            SolutionArray = charArray.Select(_chararray => _chararray - '0').ToArray();                    
-            solutionOutput += charArray[i];
+            count *= 2; 
+            SolutionArray = charArray.Select(_chararray => _chararray - '0').ToArray();
+            SolutionArray[i] += count; 
+            /*You need to make that it only reactes if you had a one, otherwise he will override zeros...*/
+            solutionOutput += SolutionArray[i];
         }
 
     }
